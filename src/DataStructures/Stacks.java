5bf5dd7f1fe0505413640f717e086099565ff32f
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-09-04 05:20:00 +0530

    Initialised Repository

---
+++ src/DataStructures/Stacks.java
@@ -0,0 +1,11 @@
+package DataStructures;
+
+public class Stacks {
+    public static void main(String[] args) {
+        // stacks = LIFO (Last in first out) type Data Structure
+        //          stores objects in sort of vertical tower
+        //          push() to add to the top
+        //          pop() to remove from the top
+
+    }
+}
