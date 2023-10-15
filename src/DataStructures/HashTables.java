cd15c1dead870ac5971f8e82fdaec234804d8ce3
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-10-16 00:52:22 +0530

    Hash Tables

--- src/DataStructures/HashTables.java
+++ src/DataStructures/HashTables.java
@@ -7,16 +7,17 @@ public class HashTables {
         Hashtable<Integer,String> table = new Hashtable<>();
         table.put(100, "Spongebob");
         table.put(102, "Patrick");
-        table.put(101, "Squidward");
+        table.put(200, "Squidward");
         table.put(103, "Sandy");
-        table.put(104, "Mr. Krabs");
+        table.put(202, "Mr. Krabs");
         System.out.println(table);
         for(Integer Key : table.keySet()) {
             System.out.println(Key + " -> " + table.get(Key));
         }
         System.out.println('\n');
         for(Integer key : table.keySet()) {
-            System.out.println(key.hashCode() + " -> " + table.get(key).hashCode());
+            System.out.println(key.hashCode() + " -> " + table.get(key).hashCode() + " -> " + table.get(key));
         }
+        System.out.println("\n" + table.keySet());
     }
 }
